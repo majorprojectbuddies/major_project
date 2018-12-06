ff93068ba64af0b4c48e76d17d33296171d9f193
sagmadan <sagarmadan5@gmail.com>
2018-12-07 02:29:16 +0530

    create update phd time table

--- Time_Table_Scheduler/src/main/java/com/sss/dao/UpdateDao.java
+++ Time_Table_Scheduler/src/main/java/com/sss/dao/UpdateDao.java
@@ -2,6 +2,7 @@ package com.sss.dao;
 
 import com.sss.classModel.FacultyResponse;
 import com.sss.classModel.FirstYearGroup;
+import com.sss.classModel.PhdGroup;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -92,7 +93,6 @@ public class UpdateDao {
 
 
 
-
     public int UpdateFirstYearInfo(ArrayList<FirstYearGroup> firstYearGroupList) {
         Connection conn = null;
         Statement stmt = null;
@@ -151,6 +151,79 @@ public class UpdateDao {
         }
         return affected;
     }
+
+
+
+
+
+
+
+    public int  UpdatePhdInfo(ArrayList<PhdGroup> phdGroupList) {
+
+
+        Connection conn = null;
+        Statement stmt = null;
+        int affected = 0;
+
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            stmt = conn.createStatement();
+
+            for(int i = 0;i<phdGroupList.size();++i){
+
+                String groupid = phdGroupList.get(i).groupId;
+                String groupid1 = groupid+"-mon";
+                String groupid2 = groupid+"-tue";
+                String groupid3 = groupid+"-wed";
+                String groupid4 = groupid+"-thu";
+                String groupid5 = groupid+"-fri";
+
+
+                String sql1 = "update phdtimetable set p89=\""+phdGroupList.get(i).timeTable.timetable[0][0]+"\",p910=\""+phdGroupList.get(i).timeTable.timetable[0][1]+"\",p1011=\""+phdGroupList.get(i).timeTable.timetable[0][2]+"\",p1112=\""+phdGroupList.get(i).timeTable.timetable[0][3]+"\",p121=\""+phdGroupList.get(i).timeTable.timetable[0][4]+"\",p12=\""+phdGroupList.get(i).timeTable.timetable[0][5]+"\",p23=\""+phdGroupList.get(i).timeTable.timetable[0][6]+"\",p34=\""+phdGroupList.get(i).timeTable.timetable[0][7]+"\",p45=\""+phdGroupList.get(i).timeTable.timetable[0][8]+"\",p56=\""+phdGroupList.get(i).timeTable.timetable[0][9]+"\" where groupid=\""+groupid1+"\"";
+                String sql2 = "update phdtimetable set p89=\""+phdGroupList.get(i).timeTable.timetable[1][0]+"\",p910=\""+phdGroupList.get(i).timeTable.timetable[1][1]+"\",p1011=\""+phdGroupList.get(i).timeTable.timetable[1][2]+"\",p1112=\""+phdGroupList.get(i).timeTable.timetable[1][3]+"\",p121=\""+phdGroupList.get(i).timeTable.timetable[1][4]+"\",p12=\""+phdGroupList.get(i).timeTable.timetable[1][5]+"\",p23=\""+phdGroupList.get(i).timeTable.timetable[1][6]+"\",p34=\""+phdGroupList.get(i).timeTable.timetable[1][7]+"\",p45=\""+phdGroupList.get(i).timeTable.timetable[1][8]+"\",p56=\""+phdGroupList.get(i).timeTable.timetable[1][9]+"\" where groupid=\""+groupid2+"\"";
+                String sql3 = "update phdtimetable set p89=\""+phdGroupList.get(i).timeTable.timetable[2][0]+"\",p910=\""+phdGroupList.get(i).timeTable.timetable[2][1]+"\",p1011=\""+phdGroupList.get(i).timeTable.timetable[2][2]+"\",p1112=\""+phdGroupList.get(i).timeTable.timetable[2][3]+"\",p121=\""+phdGroupList.get(i).timeTable.timetable[2][4]+"\",p12=\""+phdGroupList.get(i).timeTable.timetable[2][5]+"\",p23=\""+phdGroupList.get(i).timeTable.timetable[2][6]+"\",p34=\""+phdGroupList.get(i).timeTable.timetable[2][7]+"\",p45=\""+phdGroupList.get(i).timeTable.timetable[2][8]+"\",p56=\""+phdGroupList.get(i).timeTable.timetable[2][9]+"\" where groupid=\""+groupid3+"\"";
+                String sql4 = "update phdtimetable set p89=\""+phdGroupList.get(i).timeTable.timetable[3][0]+"\",p910=\""+phdGroupList.get(i).timeTable.timetable[3][1]+"\",p1011=\""+phdGroupList.get(i).timeTable.timetable[3][2]+"\",p1112=\""+phdGroupList.get(i).timeTable.timetable[3][3]+"\",p121=\""+phdGroupList.get(i).timeTable.timetable[3][4]+"\",p12=\""+phdGroupList.get(i).timeTable.timetable[3][5]+"\",p23=\""+phdGroupList.get(i).timeTable.timetable[3][6]+"\",p34=\""+phdGroupList.get(i).timeTable.timetable[3][7]+"\",p45=\""+phdGroupList.get(i).timeTable.timetable[3][8]+"\",p56=\""+phdGroupList.get(i).timeTable.timetable[3][9]+"\" where groupid=\""+groupid4+"\"";
+                String sql5 = "update phdtimetable set p89=\""+phdGroupList.get(i).timeTable.timetable[4][0]+"\",p910=\""+phdGroupList.get(i).timeTable.timetable[4][1]+"\",p1011=\""+phdGroupList.get(i).timeTable.timetable[4][2]+"\",p1112=\""+phdGroupList.get(i).timeTable.timetable[4][3]+"\",p121=\""+phdGroupList.get(i).timeTable.timetable[4][4]+"\",p12=\""+phdGroupList.get(i).timeTable.timetable[4][5]+"\",p23=\""+phdGroupList.get(i).timeTable.timetable[4][6]+"\",p34=\""+phdGroupList.get(i).timeTable.timetable[4][7]+"\",p45=\""+phdGroupList.get(i).timeTable.timetable[4][8]+"\",p56=\""+phdGroupList.get(i).timeTable.timetable[4][9]+"\" where groupid=\""+groupid5+"\"";
+
+
+                affected+=stmt.executeUpdate(sql1);
+                affected+=stmt.executeUpdate(sql2);
+                affected+=stmt.executeUpdate(sql3);
+                affected+=stmt.executeUpdate(sql4);
+                affected+=stmt.executeUpdate(sql5);
+
+            }
+
+
+            return affected;
+
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null)
+                    stmt.close();
+            } catch (SQLException se2) {
+            }
+            try {
+                if (conn != null)
+                    conn.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        return affected;
+
+    }
+
+
+
+
+
 
 
 
--- Time_Table_Scheduler/src/main/java/com/sss/services/UpdatePhdTimeTable.java
+++ Time_Table_Scheduler/src/main/java/com/sss/services/UpdatePhdTimeTable.java
@@ -1,11 +1,19 @@
 package com.sss.services;
 
 import com.sss.classModel.FirstYearGroupList;
+import com.sss.classModel.FullPhdGroup;
 import com.sss.classModel.UpdateFirstYearResponse;
+import com.sss.classModel.UpdatePhdResponse;
 import com.sss.dao.UpdateDao;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UpdatePhdTimeTable {
+    public UpdatePhdResponse updatePhdTimeTableData(FullPhdGroup fullPhdGroup){
+        UpdateDao updateDao = new UpdateDao();
+        UpdatePhdResponse updatePhdResponse = new UpdatePhdResponse();
+        updatePhdResponse.affected = Integer.toString(updateDao.UpdatePhdInfo(fullPhdGroup.fullPhdGroupList));
 
+        return updatePhdResponse;
+    }
 }
