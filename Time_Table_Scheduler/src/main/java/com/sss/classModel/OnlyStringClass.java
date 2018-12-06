68b4fb03b4719eb220ea7796f1b4a6c1b7be08b1
sagmadan <sagarmadan5@gmail.com>
2018-12-06 21:56:43 +0530

    resolved controller issues

--- Time_Table_Scheduler/src/main/java/com/sss/classModel/OnlyStringClass.java
+++ Time_Table_Scheduler/src/main/java/com/sss/classModel/OnlyStringClass.java
@@ -1,5 +1,9 @@
 package com.sss.classModel;
 
 public class OnlyStringClass {
-    public String myString;
+    public String loginid;
+
+    public OnlyStringClass(){
+        loginid="";
+    }
 }
--- Time_Table_Scheduler/src/main/java/com/sss/controller/CourseFacultyController.java
+++ Time_Table_Scheduler/src/main/java/com/sss/controller/CourseFacultyController.java
@@ -1,26 +1,27 @@
 package com.sss.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sss.classModel.OnlyStringClass;
 import com.sss.classModel.OverallResponse;
 import com.sss.dao.SignupDao;
 import com.sss.services.FetchFacultyCourseData;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @CrossOrigin
 public class CourseFacultyController {
 
-    @RequestMapping("/profile")
-    public ResponseEntity<OverallResponse> getCourseFacultyData(@RequestBody OnlyStringClass onlyStringClass){
-        String loginid = onlyStringClass.myString;
+    @PostMapping("/profile")
+    public ResponseEntity<OverallResponse> getCourseFacultyData(@RequestBody String loginid) throws IOException {
+
+        OnlyStringClass onlyStringClass = new ObjectMapper().readValue(loginid,OnlyStringClass.class);
         FetchFacultyCourseData fetchFacultyCourseData = new FetchFacultyCourseData();
         System.out.println("sagar "+ loginid);
-        return new ResponseEntity<>(fetchFacultyCourseData.fetchFacultyCourseInfo(loginid), HttpStatus.OK);
+        return new ResponseEntity<>(fetchFacultyCourseData.fetchFacultyCourseInfo(onlyStringClass.loginid), HttpStatus.OK);
 
     }
 }
