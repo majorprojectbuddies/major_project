65be0d469daf3377d9b79299bae88b9d0b6bb9e5
sagmadan <sagarmadan5@gmail.com>
2018-12-07 01:20:29 +0530

    resolved backend for profile update

--- Time_Table_Scheduler/src/main/java/com/sss/dao/UpdateDao.java
+++ Time_Table_Scheduler/src/main/java/com/sss/dao/UpdateDao.java
@@ -29,9 +29,43 @@ public class UpdateDao {
             System.out.println("Creating statement...");
             stmt = conn.createStatement();
             String sql;
-            sql = "insert into facultydetails values(\"" + facultyResponse.facultyid + "\",\"" + facultyResponse.designation + "\",\"" + facultyResponse.name + "\",\"" + facultyResponse.noOfHours + "\",\"" + facultyResponse.subject1 + "\",\"" + facultyResponse.subject2 + "\",\"" + facultyResponse.subject3 + "\")";
+            sql = "update facultydetails set name=\""+ facultyResponse.name +"\", designation=\""+facultyResponse.designation+"\",noOfHours=" + facultyResponse.noOfHours + ", subject1=\""+facultyResponse.subject1+"\",subject2=\""+facultyResponse.subject2+"\",subject3=\""+facultyResponse.subject1+"\" where facultyid=\""+facultyResponse.facultyid+"\"";
             System.out.println(sql);
             affected = stmt.executeUpdate(sql);
+
+
+
+
+
+
+
+
+
+
+            String facultyid = facultyResponse.facultyid;
+            String facultyid1 = facultyid+"-mon";
+            String facultyid2 = facultyid+"-tue";
+            String facultyid3 = facultyid+"-wed";
+            String facultyid4 = facultyid+"-thu";
+            String facultyid5 = facultyid+"-fri";
+
+
+            String sql1 = "update facultytimetable set p89=\""+facultyResponse.timeTable.timetable[0][0]+"\",p910=\""+facultyResponse.timeTable.timetable[0][1]+"\",p1011=\""+facultyResponse.timeTable.timetable[0][2]+"\",p1112=\""+facultyResponse.timeTable.timetable[0][3]+"\",p121=\""+facultyResponse.timeTable.timetable[0][4]+"\",p12=\""+facultyResponse.timeTable.timetable[0][5]+"\",p23=\""+facultyResponse.timeTable.timetable[0][6]+"\",p34=\""+facultyResponse.timeTable.timetable[0][7]+"\",p45=\""+facultyResponse.timeTable.timetable[0][8]+"\",p56=\""+facultyResponse.timeTable.timetable[0][9]+"\" where facultyttid=\""+facultyid1+"\"";
+            String sql2 = "update facultytimetable set p89=\""+facultyResponse.timeTable.timetable[1][0]+"\",p910=\""+facultyResponse.timeTable.timetable[1][1]+"\",p1011=\""+facultyResponse.timeTable.timetable[1][2]+"\",p1112=\""+facultyResponse.timeTable.timetable[1][3]+"\",p121=\""+facultyResponse.timeTable.timetable[1][4]+"\",p12=\""+facultyResponse.timeTable.timetable[1][5]+"\",p23=\""+facultyResponse.timeTable.timetable[1][6]+"\",p34=\""+facultyResponse.timeTable.timetable[1][7]+"\",p45=\""+facultyResponse.timeTable.timetable[1][8]+"\",p56=\""+facultyResponse.timeTable.timetable[1][9]+"\" where facultyttid=\""+facultyid2+"\"";
+            String sql3 = "update facultytimetable set p89=\""+facultyResponse.timeTable.timetable[2][0]+"\",p910=\""+facultyResponse.timeTable.timetable[2][1]+"\",p1011=\""+facultyResponse.timeTable.timetable[2][2]+"\",p1112=\""+facultyResponse.timeTable.timetable[2][3]+"\",p121=\""+facultyResponse.timeTable.timetable[2][4]+"\",p12=\""+facultyResponse.timeTable.timetable[2][5]+"\",p23=\""+facultyResponse.timeTable.timetable[2][6]+"\",p34=\""+facultyResponse.timeTable.timetable[2][7]+"\",p45=\""+facultyResponse.timeTable.timetable[2][8]+"\",p56=\""+facultyResponse.timeTable.timetable[2][9]+"\" where facultyttid=\""+facultyid3+"\"";
+            String sql4 = "update facultytimetable set p89=\""+facultyResponse.timeTable.timetable[3][0]+"\",p910=\""+facultyResponse.timeTable.timetable[3][1]+"\",p1011=\""+facultyResponse.timeTable.timetable[3][2]+"\",p1112=\""+facultyResponse.timeTable.timetable[3][3]+"\",p121=\""+facultyResponse.timeTable.timetable[3][4]+"\",p12=\""+facultyResponse.timeTable.timetable[3][5]+"\",p23=\""+facultyResponse.timeTable.timetable[3][6]+"\",p34=\""+facultyResponse.timeTable.timetable[3][7]+"\",p45=\""+facultyResponse.timeTable.timetable[3][8]+"\",p56=\""+facultyResponse.timeTable.timetable[3][9]+"\" where facultyttid=\""+facultyid4+"\"";
+            String sql5 = "update facultytimetable set p89=\""+facultyResponse.timeTable.timetable[4][0]+"\",p910=\""+facultyResponse.timeTable.timetable[4][1]+"\",p1011=\""+facultyResponse.timeTable.timetable[4][2]+"\",p1112=\""+facultyResponse.timeTable.timetable[4][3]+"\",p121=\""+facultyResponse.timeTable.timetable[4][4]+"\",p12=\""+facultyResponse.timeTable.timetable[4][5]+"\",p23=\""+facultyResponse.timeTable.timetable[4][6]+"\",p34=\""+facultyResponse.timeTable.timetable[4][7]+"\",p45=\""+facultyResponse.timeTable.timetable[4][8]+"\",p56=\""+facultyResponse.timeTable.timetable[4][9]+"\" where facultyttid=\""+facultyid5+"\"";
+
+            System.out.println(sql1);
+
+            affected+=stmt.executeUpdate(sql1);
+            affected+=stmt.executeUpdate(sql2);
+            affected+=stmt.executeUpdate(sql3);
+            affected+=stmt.executeUpdate(sql4);
+            affected+=stmt.executeUpdate(sql5);
+
+
+
             return affected;
 
         } catch (SQLException se) {
